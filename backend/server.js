07d813d56b970fd8ea978c7c5513b474d8952fcf
Jaiveer yadav <dev.jaiveer.yadav@gmail.com>
2021-11-26 13:52:43 +0530

    added mailgun to backend

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 app.use('/api/orders', orderRouter);
 app.get('/api/config/paypal', (req, res) => {
-  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+  res.send(process.env.PAYPAL_CLIENT_ID || 'putYourKeyHere');
 });
 app.get('/api/config/google', (req, res) => {
   res.send(process.env.GOOGLE_API_KEY || '');
