238a0dfcb8ff51c9f08c3c86ced44dc6fe96664c
Jaiveer yadav <dev.jaiveer.yadav@gmail.com>
2021-11-26 15:37:44 +0530

    added paypal key

--- backend/config.js
+++ backend/config.js
@@ -6,7 +6,7 @@ export default {
   PORT: process.env.PORT || 5000,
   MONGODB_URL:'mongodb+srv://tenreck:tenreck@123@tenreck.ysmix.mongodb.net/Tenreck?retryWrites=true&w=majority',
   JWT_SECRET: process.env.JWT_SECRET || 'ILoveEverythingTheWorldIsCurrepted0034',
-  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'putYourKeyHere',
+  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'AXCn43bR2htx6LJ6BlnsTZeJmK23Pt77xZuPhGXJepmenW2AeAX1Ei9owV5rxsO2IEEDpk8qJb__YafD',
   accessKeyId: process.env.accessKeyId || 'IHaveNoIdea',
   secretAccessKey: process.env.secretAccessKey || 'KeyIsNotJustToOpenLocks',
 };
