27c4247e261477a62e026f8a1108740afcc3ffec
BuildTools <vlad@vladedits.com>
2021-12-04 18:17:14 +0200

    commit to see if the invalid email would go away

--- frontend/src/components/Axios.js
+++ frontend/src/components/Axios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // this connects the backend
+  //making this commit to see if the invalid email would go away
   baseURL: "https://api.tenreck.com",
 });
 
