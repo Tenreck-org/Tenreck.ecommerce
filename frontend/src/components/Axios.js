9b02b502eb997ca60a69ae94dd76c136bb0509fc
BuildTools <vlad@vladedits.com>
2021-12-04 20:04:01 +0200

    changed axios back to normal

--- frontend/src/components/Axios.js
+++ frontend/src/components/Axios.js
@@ -2,8 +2,6 @@ import axios from "axios";
 
 const instance = axios.create({
   // this connects the backend
-  //making this commit to see if the invalid email would go away
-  //gpg key test1
   baseURL: "https://api.tenreck.com",
 });
 
