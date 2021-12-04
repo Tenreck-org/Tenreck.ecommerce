e44d07198a9ffe26a741c85e15331d81bec2d9d8
BuildTools <vlad@vladedits.com>
2021-12-04 19:45:41 +0200

    gpg key test

--- frontend/src/components/Axios.js
+++ frontend/src/components/Axios.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   // this connects the backend
   //making this commit to see if the invalid email would go away
+  //gpg key test
   baseURL: "https://api.tenreck.com",
 });
 
