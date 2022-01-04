d0e6b0e7b698f32f64f08a45ab458876aad0bb58
AaryanRachit <97021562+AaryanRachit@users.noreply.github.com>
2022-01-04 18:30:10 +0530

    Test
    Test

--- backend/routes/striperoute.js
+++ backend/routes/striperoute.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import StripeCheckout from "react-stripe-checkout"
-function stripe() {
-const [product] = React.useState({
+function Stripe() {
+const [product] = useState({
     name: "Dynamic Website",
     price: "6.00",
 })
@@ -11,8 +11,7 @@ function handleToken(token, addresses) {
     return (
         <div className='container'>
             <div className='product'>
-       <h1>{product.name}</h1>
-       <h3>${product.price}</h3>
+       
             </div>
             <StripeCheckout 
             stripeKey='pk_test_51KDqg6CHVE7fxEodl310AvgPAjlV4gKFRpaSA2vSCkyTf2YEhrELGLznPmlcBkJVillyrocvD4du8dBuhhHqJP6g00OPykh47W'
@@ -22,4 +21,4 @@ function handleToken(token, addresses) {
     )
 }
 
-export default stripe
+export default Stripe
