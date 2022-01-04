78d4329b581944dc5a1e562c99bdaa3dbb8fd3ba
AaryanRachit <97021562+AaryanRachit@users.noreply.github.com>
2022-01-04 18:39:29 +0530

    Stripe backend
    Stripe backend

--- backend/routes/striperoute.js
+++ backend/routes/striperoute.js
@@ -1,24 +1,73 @@
-import React from 'react'
-import StripeCheckout from "react-stripe-checkout"
-function Stripe() {
-const [product] = useState({
-    name: "Dynamic Website",
-    price: "6.00",
-})
-function handleToken(token, addresses) {
- 
-}
-    return (
-        <div className='container'>
-            <div className='product'>
-       
-            </div>
-            <StripeCheckout 
-            stripeKey='pk_test_51KDqg6CHVE7fxEodl310AvgPAjlV4gKFRpaSA2vSCkyTf2YEhrELGLznPmlcBkJVillyrocvD4du8dBuhhHqJP6g00OPykh47W'
-            token={handleToken}
-            />
-        </div>
-    )
-}
+import  express from 'express';
 
-export default Stripe
+const cors = require ("cors")
+const express = require("express");
+const stripe = require ("stripe")
+("sk_test_51KDqg6CHVE7fxEodfAFi2l9PNj2s1kOmhzhJxSk0GwN7gfFte7VWbJECRIClXJdWPeLJLlBDHg4a79nagRbz04e500gICUCx8l");
+const uuid = require(uuid/v4);
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Add your Stripe Secret Key to the .require('stripe') statement!");
+});
+
+app.post("/checkout", async (req, res) => {
+  console.log("Request:", req.body);
+
+  let error;
+  let status;
+  try {
+    const { product, token } = req.body;
+
+    const customer = await stripe.customers.create({
+      email: token.email,
+      source: token.id
+    });
+
+    const idempotency_key = uuid();
+    const charge = await stripe.charges.create(
+      {
+        amount: product.price * 100,
+        currency: "usd",
+        customer: customer.id,
+        receipt_email: token.email,
+        description: `Purchased the ${product.name}`,
+        shipping: {
+          name: token.card.name,
+          address: {
+            line1: token.card.address_line1,
+            line2: token.card.address_line2,
+            city: token.card.address_city,
+            country: token.card.address_country,
+            postal_code: token.card.address_zip
+          }
+        }
+      },
+      {
+        idempotency_key
+      }
+    );
+    console.log("Charge:", { charge });
+    status = "success";
+  } catch (error) {
+    console.error("Error:", error);
+    status = "failure";
+  }
+
+  res.json({ error, status });
+});
+
+app.listen(8080);
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
