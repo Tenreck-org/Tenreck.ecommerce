6510cb0b0716025d0d4a11668a041e166bfda5f3
ayan-afk <79571638+ayan-afk@users.noreply.github.com>
2022-02-09 10:10:20 +0530

    Update Hiring.js

--- frontend/src/Screens/Hiring.js
+++ frontend/src/Screens/Hiring.js
@@ -8,10 +8,10 @@ function Hiring() {
         <h2 className='heading2'>We are the first to sell websites with hosting and domains in the world,<br /> and our independence from other organizations and institutions will provide<br /> you with unprecedented freedom to create, learn, and contribute. Whether your<br /> interests lie in business, community, or developement, join us today and empower <br /> our generation to revolutionize our future!</h2>
         <br />
         <hr />
-        <br />
             <h1 className="heading1">Jobs</h1>
-            <br />
             <Cardjob name='Software Developer' url='https://cdn.discordapp.com/attachments/749964857524224060/937964620130418688/2842680.jpg'/>
+            <Cardjob name='Tester' url='https://cdn.discordapp.com/attachments/749964857524224060/937964620130418688/2842680.jpg'/>
+            <Cardjob name='Marketing Officer' url='https://cdn.discordapp.com/attachments/749964857524224060/938432798807715930/547.jpg'/>
             <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><hr />
       </div>
   );
